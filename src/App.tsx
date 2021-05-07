fbc86da5fc84f7e16df7e75a1656432952f7b2ea
Malith Kuruppu <info.mkappworks@gmail.com>
2021-05-07 22:27:46 +0530

    updated design

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import React, { useState } from "react";
+
+import { v4 as uuidv4 } from "uuid";
+
+import { Row, Col, Card, PageHeader, message } from "antd";
+import "antd/dist/antd.css";
+import "./App.less";
+
 import { TodoList } from "./components/TodoList";
 import { AddTodoForm } from "./components/AddTodoForm";
-import { DatePicker } from "antd";
 
 const initialTodos: Todo[] = [
   {
+    id: "1",
     text: "Walk the dog",
     complete: false,
   },
   {
+    id: "2",
     text: "Write app",
     complete: true,
   },
@@ -28,19 +36,57 @@ function App() {
       return todo;
     });
     setTodos(newTodos);
+    message.info("Todo state updated!");
+  };
+
+  const removeTodo: RemoveTodo = (selectedTodo: Todo) => {
+    const newTodos = todos.filter((todo) => todo.id !== selectedTodo.id);
+
+    setTodos(newTodos);
+    message.warn("Todo removed!");
   };
 
   const addTodo: AddTodo = (text: string) => {
-    const newTodo = { text, complete: false };
+    const newTodo = { id: uuidv4(), text, complete: false };
     setTodos([...todos, newTodo]);
+    message.success("Todo added!");
   };
 
   return (
-    <>
-      <TodoList todos={todos} toggleTodo={toggleTodo} />
-      <AddTodoForm addTodo={addTodo} />
-      <DatePicker />
-    </>
+    <Row
+      justify="center"
+      align="middle"
+      gutter={[0, 20]}
+      className="todos-container"
+    >
+      <Col
+        xs={{ span: 23 }}
+        sm={{ span: 23 }}
+        md={{ span: 21 }}
+        lg={{ span: 20 }}
+        xl={{ span: 18 }}
+      >
+        <Card title="Create a new todo">
+          <AddTodoForm addTodo={addTodo} />
+        </Card>
+      </Col>
+
+      <Col
+        xs={{ span: 23 }}
+        sm={{ span: 23 }}
+        md={{ span: 21 }}
+        lg={{ span: 20 }}
+        xl={{ span: 18 }}
+      >
+        <Card title="Todo List">
+          <TodoList
+            todos={todos}
+            toggleTodo={toggleTodo}
+            removeTodo={removeTodo}
+          />
+        </Card>
+      </Col>
+    </Row>
   );
 }
 

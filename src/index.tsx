5b12c5c4041e76e752a4c2427a9485b0e1603798
Malith Kuruppu <info.mkappworks@gmail.com>
2021-05-07 22:26:45 +0530

    added styling to the index file

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import './base.less';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+

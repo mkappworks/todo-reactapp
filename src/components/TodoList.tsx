e23b5306b79f1dcbb378aadec8f73fc989ca3bf0
Malith Kuruppu <info.mkappworks@gmail.com>
2021-05-07 22:28:45 +0530

    updated todolist design

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
-import { TodoListItem } from './TodoListItem';
+import React from "react";
+import { TodoListItem } from "./TodoListItem";
 
 interface Props {
   todos: Todo[];
   toggleTodo: ToggleTodo;
+  removeTodo: RemoveTodo;
 }
 
-export const TodoList: React.FC<Props> = ({ todos, toggleTodo }) => {
+export const TodoList: React.FC<Props> = ({
+  todos,
+  toggleTodo,
+  removeTodo,
+}) => {
   return (
     <ul>
-      {todos.map(todo => (
-        <TodoListItem key={todo.text} todo={todo} toggleTodo={toggleTodo} />
+      {todos.map((todo) => (
+        <TodoListItem
+          key={todo.id}
+          todo={todo}
+          removeTodo={removeTodo}
+          toggleTodo={toggleTodo}
+        />
       ))}
     </ul>
   );
--- src/components/TodoListItem.tsx
+++ src/components/TodoListItem.tsx
@@ -1,25 +1,71 @@
 import React from "react";
+import { Tooltip, Tag, List, Button, Popconfirm, Switch } from "antd";
+import { CloseOutlined, CheckOutlined } from "@ant-design/icons";
+
+import "./TodoListItem.less";
 
 interface Props {
   todo: Todo;
   toggleTodo: ToggleTodo;
+  removeTodo: RemoveTodo;
 }
 
-export const TodoListItem: React.FC<Props> = ({ todo, toggleTodo }) => {
+// export const TodoListItem: React.FC<Props> = ({ todo, toggleTodo }) => {
+//   return (
+//     <li>
+//       <label
+//         style={{ textDecoration: todo.complete ? "line-through" : undefined }}
+//       >
+//         <input
+//           type="checkbox"
+//           checked={todo.complete}
+//           onClick={() => {
+//             toggleTodo(todo);
+//           }}
+//         />{" "}
+//         {todo.text}
+//       </label>
+//     </li>
+//   );
+// };
+
+export const TodoListItem: React.FC<Props> = ({
+  todo,
+  toggleTodo,
+  removeTodo,
+}) => {
   return (
-    <li>
-      <label
-        style={{ textDecoration: todo.complete ? "line-through" : undefined }}
-      >
-        <input
-          type="checkbox"
-          checked={todo.complete}
-          onClick={() => {
-            toggleTodo(todo);
+    <List.Item
+      actions={[
+        <Tooltip
+          title={todo.complete ? "Mark as uncompleted" : "Mark as completed"}
+        >
+          <Switch
+            checkedChildren={<CheckOutlined />}
+            unCheckedChildren={<CloseOutlined />}
+            onChange={() => toggleTodo(todo)}
+            defaultChecked={todo.complete}
+          />
+        </Tooltip>,
+        <Popconfirm
+          title="Are you sure you want to delete?"
+          onConfirm={() => {
+            removeTodo(todo);
           }}
-        />{" "}
-        {todo.text}
-      </label>
-    </li>
+        >
+          <Button className="remove-todo-button" type="primary" danger>
+            X
+          </Button>
+        </Popconfirm>,
+      ]}
+      className="list-item"
+      key={todo.id}
+    >
+      <div className="todo-item">
+        <Tag color={todo.complete ? "cyan" : "red"} className="todo-tag">
+          {todo.text}
+        </Tag>
+      </div>
+    </List.Item>
   );
 };
